970fc2d369ea4f060b23605e55aefce016fcb6c5
jcbarcelosrj <jcbarcelosrj@gmail.com>
2024-09-13 16:09:31 -0300

    feat: add validation in pom.xml

--- crud-spring/src/main/java/com/rasec23rj/crud_spring/models/Courses.java
+++ crud-spring/src/main/java/com/rasec23rj/crud_spring/models/Courses.java
@@ -1,5 +1,7 @@
 package com.rasec23rj.crud_spring.models;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -14,10 +16,14 @@ public class Courses {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @Column(nullable = false, length = 200)
+    @Column(nullable = false, length = 100)
+    @NotNull(message = "Name cannot be null")
+    @Size(min = 5, max = 100, message = "Name must be between 5 and 100 characters")
     private String name;
 
     @Column(nullable = false, length = 200)
+    @NotNull(message = "Category cannot be null")
+    @Size(min = 5, max = 100, message = "Category must be between 5 and 100 characters")
     private String category;
 
 }
