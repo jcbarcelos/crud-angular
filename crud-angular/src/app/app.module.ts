b41849178c820666c40c113d5eba77f5c7f3e92a
jcbarcelosrj <jcbarcelosrj@gmail.com>
2024-09-26 09:46:37 -0300

    feat: upgrade 16 to 17 cdk

--- crud-angular/src/app/app.module.ts
+++ crud-angular/src/app/app.module.ts
@@ -6,22 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses/courses.component';
 import { SharedModule } from './shared/shared.module';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { CoursesModule } from './courses/courses.module';
 
-@NgModule({
-  declarations: [AppComponent, CoursesComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    SharedModule,
-    CoursesModule,
-  ],
-  providers: [],
-  exports: [SharedModule],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  bootstrap: [AppComponent],
-})
+@NgModule({ declarations: [AppComponent, CoursesComponent],
+    exports: [SharedModule],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        SharedModule,
+        CoursesModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
